4ee7daa15ed4b91087f15575c65f56129728cb1e
Neri Montiel <neri_britney260500@outlook.com>
2025-02-01 01:10:16 -0600

    Version_02: Instrucciones basicas en boton y caja de texto.

--- DAM_Leccion_NBMR/MainPage.xaml.cs
+++ DAM_Leccion_NBMR/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_NBMR
+﻿using DAM_Leccion_NBMR.Model;
+
+namespace DAM_Leccion_NBMR
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            Ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,22 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar()
+        {
+            PersonaModel personaModel = new PersonaModel();
+            personaModel.Nombre = "Hola aqui estoy";
+            txtNombre.Text = personaModel.Nombre;
+           
+            
+            //txtNombre.Text = "HOLA ESTOY AQUI";
+        }
+
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema", "Se ha guardado el registro en la BD", "Aceptar");
+
         }
     }
 
